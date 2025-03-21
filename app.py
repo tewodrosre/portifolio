bac1a56a217f35c912f26500bbee8df281001780
tewodrosre <tewodrosre@gmail.com>
2025-03-21 10:34:08 +0300

    portifolio

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
+
+@app.route('/certifications')
+def certifications():
+    return render_template('certifications.html')
+
+if __name__ == '__main__':
+    app.run()
